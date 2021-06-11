0c784a9b8dc5e4b971033d3d8c22677c7401ddae
Naushad Alam <naushad@ProM1.local>
2021-06-11 11:12:53 +0100

    Adding config file

---
+++ config.py
@@ -0,0 +1,18 @@
+threshold={
+    'word_count_thres': 2,
+    'char_count_thres': 10,
+}
+
+paths={
+    'out_path': '/Users/naushad/Documents/memexqa/prepro_v1.1/',
+    'shared_path': '/Users/naushad/Documents/memexqa/shared_out/'
+}
+
+feature_dim={
+    'image_feat_dim':3048
+}
+
+model_params = {
+
+}
+
