c9b50137d53fdea4425d7c5def962622f0a3895d
Naushad Alam <naushad@ProM1.local>
2021-06-17 13:28:09 +0100

    Adding more model params

--- config.py
+++ config.py
@@ -1,15 +1,28 @@
 
-threshold={
+params= {
     'word_count_thres': 2,
     'char_count_thres': 10,
+    'max_num_albums': 8,
+    'max_num_photos': 10,
+    'max_sent_title_size': 35,
+    'max_sent_des_size': 2574,
+    'max_when_size': 4,
+    'max_where_size': 10,
+    'max_answer_size': 18,
+    'max_question_size': 25,
+    'max_word_size': 42,
+    'image_feat_dim': 3048,
+    'gpu': 'gpu0',
+    'learning_rate': 0.01,
+    'exp_decay_rate': 0.999,
 }
 
-paths={
+paths = {
     'out_path': '/Users/naushad/Documents/memexqa/prepro_v1.1/',
     'shared_path': '/Users/naushad/Documents/memexqa/shared_out/'
 }
 
-feature_dim={
+feature_dim = {
     'image_feat_dim':3048
 }
 
