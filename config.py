cf620c76e46685e742bfd50435bfecde2e706132
Naushad Alam <naushad@ProM1.local>
2021-07-05 17:25:19 +0100

    Restructuring code

--- config.py
+++ config.py
@@ -2,14 +2,17 @@
 params = {
     'load': False,
     'is_train': True,
-    'image_feat_dim': 2537,
 
+    # Hyper Params
     'gpu': 'gpu0',
     'learning_rate': 0.01,
     'exp_decay_rate': 0.999,
     'dropout': 0.2,
     'num_epochs': 5,
+    'hidden_size': 100,
+    'batch_size': 6,
 
+    # Model agnostic thresholds - verify before run
     'word_count_thres': 2,
     'char_count_thres': 10,
     'num_albums_thres': 4,
@@ -23,15 +26,12 @@ params = {
     'question_size_thres': 25,
     'word_size_thres': 16,
 
-    'hidden_size': 100,
-    'batch_size': 6,
-
     'char_emb_size': 100,
     'char_out_size': 100,
     'image_trans_dim': 100,
-
     'char_channel_size': 100,
     'char_channel_width': 5,
+    'image_feat_dim': 2537,
 }
 
 paths = {
